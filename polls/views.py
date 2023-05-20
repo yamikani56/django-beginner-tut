d717fef8a76eb17a8572ea2f92bcf8f4bde5d001
yamikani mnenula <yamikanijohnmnenula@gmail.com>
2023-05-20 20:40:03 +0200

    updated index.html in templates folder, in views.py ive updated to code to just use render function in displaying the index.html template

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello from polls view")
+    latest_question_list=Question.objects.order_by("-pub_date")[:5]
+    output=", ".join([q.question_text for q in latest_question_list])
+    # template = loader.get_template("polls/index.html")
+    context ={
+        "latest_question_list":latest_question_list
+    }
+    # return HttpResponse(template.render(context,request))
+
+    return render(request,"polls/index.html",context)
 
 
 def details(request,question_id):
